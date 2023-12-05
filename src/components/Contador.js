ea4dc5c145bd404f5f8c62c3e676e9a424ecccd1
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-12-05 17:35:12 +0000

    Added Contador component to display click count

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "../stylesheets/Contador.css";
 
-// Creando una clase para el componente Contador
-/* function Contador(props) {
-  return(
-    <div className="contador">
-      {props.numClics}
-    </div>
-  )
-} */
-
-// *Creando componente Contador con SINTAXIS DE DESESTRUCTURACION
-// ---entre {} se escribe la propiedad del objeto que se va a pasar
-function Contador({ numClics }) {
-  return <div className="contador">{numClics}</div>;
+// CREANDO COMPONENTE DE CLASE
+class Contador extends React.Component {
+  render() {
+    return <div className="contador">{this.props.numClics}</div>;
+  }
 }
 
 export default Contador;
